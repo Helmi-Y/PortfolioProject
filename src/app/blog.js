b60adfa62ea98244ae318a6ca97053f440362e27
Helmi-Y <helmi.yatim7@gmail.com>
2025-01-27 12:20:08 -0600

    Add files via upload

---
+++ src/app/blog.js
@@ -0,0 +1,14 @@
+// portfolio/app/blog.js
+import React from 'react';
+import BlogApp from '../../blog/app.js'; // Import the blog app from the blog folder
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <BlogApp />
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>EJS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>C#</li>
+        <li>MongoDB</li>
+
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>GEMS World Academy Singapore</li>
+        <li>Monterey Peninsula College</li>
+        <li>Dakota State University</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Awards",
+    id: "awards",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Head of School Award</li>
+        <li>Indonesia U-18 Baseball National Team</li>
+        <li>MPC Honors List</li>
+        <li>DSU Honors List</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about.png"  alt="about me"   width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer and a baseball athlete 
+            currently studying at Dakota State University. I have experience
+            working with JavaScript, React, Node.js, Express, EJS, MongoDB,
+            HTML, CSS, and Git. I am a quick learner and I always thrive to expand
+            my skillsets and my knowldege. Growing up, I played a lot of team sports.
+            Thus, I am a team player and I love working with others to create
+            applications and be successful.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("awards")}
+              active={tab === "awards"}
+            >
+              {" "}
+              Awards{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
